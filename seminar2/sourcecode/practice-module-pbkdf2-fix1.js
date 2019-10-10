bef76c699adcd851a980b8de942c0017f7dde21a
윤희성 <heesung6701@naver.com>
2019-10-10 13:18:45 +0900

    pbkdf2 module 수정

--- seminar2/sourcecode/practice-module-pbkdf2-fix1.js
+++ seminar2/sourcecode/practice-module-pbkdf2-fix1.js
@@ -1,12 +1,11 @@
 const crypto = require('crypto');
-const pbkdf2 = require('pbkdf2');
 const fs = require('fs');
 
 const password = 'password1234';
 crypto.randomBytes(32, madeSaltFunc);
 function madeSaltFunc(err, salt) {
     if(err) throw err;
-    pbkdf2.pbkdf2(password, salt, 1, 32, 'sha512', madeKeyFunc);
+    crypto.pbkdf2(password, salt, 1, 32, 'sha512', madeKeyFunc);
 }
 function madeKeyFunc(err, derivedKey) {
     if(err) throw err;
--- seminar2/sourcecode/practice-module-pbkdf2-sync.js
+++ seminar2/sourcecode/practice-module-pbkdf2-sync.js
@@ -1,9 +1,8 @@
 const crypto = require('crypto');
-const pbkdf2 = require('pbkdf2');
 const fs = require('fs');
 
 const password = 'password';
 const salt = crypto.randomBytes(32);
-const derivedKey = pbkdf2.pbkdf2Sync(password, salt, 1, 32, 'sha512');
+const derivedKey = crypto.pbkdf2Sync(password, salt, 1, 32, 'sha512');
 fs.writeFileSync('password.txt', derivedKey.toString('hex'));
 console.log('complete write password');
