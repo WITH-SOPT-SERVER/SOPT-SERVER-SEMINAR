d5ad70f2d6226eeea656bd9c86cfb8239549fd39
윤희성 <heesung6701@naver.com>
2019-10-31 14:15:04 +0900

    model 수정

--- seminar4/practice4/model/board.js
+++ seminar4/practice4/model/board.js
@@ -9,116 +9,111 @@ module.exports = {
         content,
         writerIdx
     }) => {
-        return new Promise((resolve, reject) => {
-            const table = 'board';
-            const fields = 'title, content, writerIdx';
-            const questions = `?, ?, ?`;
-            const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
-            const values = [title, content, writerIdx];
-            pool.queryParam_Parse(query, values)
-                .then(result => {
-                    console.log(result);
-                    const boardId = result.boardId;
-                    resolve({
-                        code: statusCode.OK,
-                        json: authUtil.successTrue(responseMessage.BOARD_CREATE_SUCCESS, boardId)
-                    });
-                })
-                .catch(err => {
-                    // ER_NO_REFERENCED_ROW_2
-                    if(err.errno == 1452){
-                        resolve({
-                            code: statusCode.BAD_REQUEST,
-                            json: authUtil.successFalse([responseMessage.BOARD_CREATE_FAIL,responseMessage.NO_USER].join(','))
-                        })
-                        console.log(err.errno, err.code);
-                        return;
-                    }
-                    console.log(err);
-                    reject(err);
-                });
-        })
+        const table = 'board';
+        const fields = 'title, content, writerIdx';
+        const questions = `?, ?, ?`;
+        const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
+        const values = [title, content, writerIdx];
+        return pool.queryParam_Parse(query, values)
+            .then(result => {
+                console.log(result);
+                const boardId = result.boardId;
+                return {
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(responseMessage.BOARD_CREATE_SUCCESS, boardId)
+                };
+            })
+            .catch(err => {
+                // ER_NO_REFERENCED_ROW_2
+                if (err.errno == 1452) {
+                    console.log(err.errno, err.code);
+                    return {
+                        code: statusCode.BAD_REQUEST,
+                        json: authUtil.successFalse([responseMessage.BOARD_CREATE_FAIL, responseMessage.NO_USER].join(','))
+                    };
+                }
+                console.log(err);
+                throw err;
+            });
     },
     readAll: () => {
-        return new Promise((resolve, reject) => {
-            const table = 'board';
-            const query = `SELECT * FROM ${table}`
-            pool.queryParam_None(query)
-                .then(result => {
-                    resolve({
-                        code: statusCode.OK,
-                        json: authUtil.successTrue(responseMessage.BOARD_READ_SUCCESS, result)
-                    });
-                })
-                .catch(err => {
-                    reject(err);
-                });
-        })
+        const table = 'board';
+        const query = `SELECT * FROM ${table}`
+        return pool.queryParam_None(query)
+            .then(result => {
+                return {
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(responseMessage.BOARD_READ_SUCCESS, result)
+                };
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
     },
-    read: ({boardIdx}) => {
-        return new Promise((resolve, reject) => {
-            const table = 'board';
-            const query = `SELECT * FROM ${table} WHERE boardIdx = '${boardIdx}'`;
-            pool.queryParam_None(query)
-                .then(result => {
-                    if(result.length == 0){
-                        resolve({
-                            code: statusCode.BAD_REQUEST,
-                            json: authUtil.successFalse(responseMessage.NO_BOARD)
-                        })
-                        return;
-                    }
-                    resolve({
-                        code: statusCode.OK,
-                        json: authUtil.successTrue(responseMessage.BOARD_READ_SUCCESS, result[0])
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    reject(err);
-                });
-        })
+    read: ({
+        boardIdx
+    }) => {
+        const table = 'board';
+        const query = `SELECT * FROM ${table} WHERE boardIdx = '${boardIdx}'`;
+        return pool.queryParam_None(query)
+            .then(result => {
+                if (result.length == 0) {
+                    return {
+                        code: statusCode.BAD_REQUEST,
+                        json: authUtil.successFalse(responseMessage.NO_BOARD)
+                    };
+                }
+                return {
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(responseMessage.BOARD_READ_SUCCESS, result[0])
+                };
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
     },
-    update: ({boardIdx, title, content}) => {
-        return new Promise((resolve, reject) => {
-            const table = 'board';
-            const conditions = [];
-            if(title) conditions.push(`title = '${title}'`);
-            if(content) conditions.push(`content = '${content}'`);
-            const setStr = conditions.length > 0 ? `SET ${conditions.join(',')}` : '';
-            const query = `UPDATE ${table} ${setStr} WHERE boardIdx = ${boardIdx}`;
-            pool.queryParam_None(query)
-                .then(result => {
-                    console.log(result);
-                    resolve({
-                        code: statusCode.OK,
-                        json: authUtil.successTrue(responseMessage.BOARD_UPDATE_SUCCESS)
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    reject(err);
-                });
-        })
+    update: ({
+        boardIdx,
+        title,
+        content
+    }) => {
+        const table = 'board';
+        const conditions = [];
+        if (title) conditions.push(`title = '${title}'`);
+        if (content) conditions.push(`content = '${content}'`);
+        const setStr = conditions.length > 0 ? `SET ${conditions.join(',')}` : '';
+        const query = `UPDATE ${table} ${setStr} WHERE boardIdx = ${boardIdx}`;
+        return pool.queryParam_None(query)
+            .then(result => {
+                console.log(result);
+                return {
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(responseMessage.BOARD_UPDATE_SUCCESS)
+                };
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
     },
     delete: (whereJson = {}) => {
-        return new Promise((resolve, reject) => {
-            const table = 'board';
-            const conditions = Object.entries(whereJson).map(it => `${it[0]} = '${it[1]}'`).join(','); 
-            const whereStr = conditions.length > 0 ? `WHERE ${conditions}` : '';
-            const query = `DELETE FROM ${table} ${whereStr}`
-            pool.queryParam_None(query)
-                .then(result => {
-                    console.log(result);
-                    resolve({
-                        code: statusCode.OK,
-                        json: authUtil.successTrue(responseMessage.BOARD_DELETE_SUCCESS)
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    reject(err);
-                });
-        })
+        const table = 'board';
+        const conditions = Object.entries(whereJson).map(it => `${it[0]} = '${it[1]}'`).join(',');
+        const whereStr = conditions.length > 0 ? `WHERE ${conditions}` : '';
+        const query = `DELETE FROM ${table} ${whereStr}`
+        return pool.queryParam_None(query)
+            .then(result => {
+                console.log(result);
+                return {
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(responseMessage.BOARD_DELETE_SUCCESS)
+                };
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
     },
 };
--- seminar4/practice4/model/user.js
+++ seminar4/practice4/model/user.js
@@ -2,87 +2,78 @@ const authUtil = require('../module/authUtil');
 const statusCode = require('../module/statusCode');
 const responseMessage = require('../module/responseMessage');
 const pool = require('../module/poolAsync');
+const encrypt = require('../module/encryption');
 
 module.exports = {
     signin: ({
         id,
         password
     }) => {
-        return new Promise(async (resolve, reject) => {
-            const table = 'user';
-            const query = `SELECT * FROM ${table} WHERE id = '${id}'`;
-            pool.queryParam_None(query)
-                .then(userResult => {
-                    console.log(userResult);
-                    if (userResult.length == 0) {
-                        resolve({
-                            code: statusCode.BAD_REQUEST,
-                            json: authUtil.successFalse(responseMessage.NO_USER)
-                        });
-                        return;
-                    }
-                    const user = userResult[0];
-                    // 비밀번호 체크
-                    if (user.password != password) {
-                        resolve({
-                            code: statusCode.BAD_REQUEST,
-                            json: authUtil.successFalse(responseMessage.MISS_MATCH_PW)
-                        })
-                        return;
-                    }
-                    // 로그인 성공
-                    resolve({
-                        code: statusCode.OK,
-                        json: authUtil.successTrue(responseMessage.SIGN_IN_SUCCESS)
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    reject(err);
-                });
-        })
+        const table = 'user';
+        const query = `SELECT * FROM ${table} WHERE id = '${id}'`;
+        return pool.queryParam_None(query)
+            .then(async (userResult) => {
+                console.log(userResult);
+                if (userResult.length == 0) {
+                    return {
+                        code: statusCode.BAD_REQUEST,
+                        json: authUtil.successFalse(responseMessage.NO_USER)
+                    };
+                }
+                const user = userResult[0];
+                // 비밀번호 체크
+                const {
+                    hashed
+                } = await encrypt.encryptWithSalt(password, user.salt);
+                if (user.password != hashed) {
+                    return {
+                        code: statusCode.BAD_REQUEST,
+                        json: authUtil.successFalse(responseMessage.MISS_MATCH_PW)
+                    };
+                }
+                // 로그인 성공
+                return {
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(responseMessage.SIGN_IN_SUCCESS)
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
     },
     signup: ({
         id,
         password,
+        salt,
         name,
         email,
         phone
     }) => {
-        return new Promise(async (resolve, reject) => {
-            const table = 'user';
-            const query = `SELECT * FROM ${table} WHERE id = '${id}'`;
-            pool.queryParam_None(query)
-                .then(userResult => {
-                    // 아이디 중복 체크
-                    if (userResult.length > 0) {
-                        resolve({
-                            code: statusCode.BAD_REQUEST,
-                            json: authUtil.successFalse(responseMessage.ALREADY_ID)
-                        })
-                        return;
-                    }
-                    const table = 'user';
-                    const fields = 'id, name, password, salt, email, phone';
-                    const questions = `?, ?, ?, ?, ?, ?`;
-                    const values = [id, name, password, ' ', email, phone];
-                    pool.queryParam_Parse(`INSERT INTO ${table}(${fields}) VALUES(${questions})`, values)
-                        .then(result => {
-                            const userId = result.insertId;
-                            resolve({
-                                code: statusCode.OK,
-                                json: authUtil.successTrue(responseMessage.SIGN_UP_SUCCESS, userId)
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            reject(err);
-                        });
-                })
-                .catch(err => {
-                    console.log(err);
-                    reject(err);
-                });
-        })
+        const table = 'user';
+        const fields = 'id, name, password, salt, email, phone';
+        const questions = `?, ?, ?, ?, ?, ?`;
+        const values = [id, name, password, salt, email, phone];
+        return pool.queryParam_Parse(`INSERT INTO ${table}(${fields}) VALUES(${questions})`, values)
+            .then(result => {
+                if (result.code && result.json) return result;
+                const userId = result.insertId;
+                return {
+                    code: statusCode.OK,
+                    json: authUtil.successTrue(responseMessage.SIGN_UP_SUCCESS, userId)
+                };
+            })
+            .catch(err => {
+                // ER_DUP_ENTRY
+                if(err.errno == 1062){
+                    console.log(err.errno, err.code);
+                    return {
+                        code: statusCode.BAD_REQUEST,
+                        json: authUtil.successFalse(responseMessage.ALREADY_ID)
+                    };
+                }
+                console.log(err);
+                throw err;
+            });
     }
 };
