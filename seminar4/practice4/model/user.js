3dae6d9e7f92e998683bdd10f187dce2d92752e5
윤희성 <heesung6701@naver.com>
2019-10-31 12:48:51 +0900

    model 구현

--- seminar4/practice4/model/user.js
+++ seminar4/practice4/model/user.js
@@ -1,63 +1,88 @@
 const authUtil = require('../module/authUtil');
 const statusCode = require('../module/statusCode');
 const responseMessage = require('../module/responseMessage');
-
-const userDB = [];
+const pool = require('../module/poolAsync');
 
 module.exports = {
-    signin: (id, pwd) => {
-        return new Promise((resolve, reject) => {
-             // 유저가 존재하는지 체크
-            const user = userDB.find(it => it.id == id);
-            console.log(user);
-            if(!user){
-                resolve({
-                    code: statusCode.BAD_REQUEST,
-                    json: authUtil.successFalse(responseMessage.NO_USER)
-                });
-                return;
-            }
-            // 비밀번호 체크
-            if(user.pwd != pwd){
-                resolve({
-                    code: statusCode.BAD_REQUEST,
-                    json: authUtil.successFalse(responseMessage.MISS_MATCH_PW)
+    signin: ({
+        id,
+        password
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            const table = 'user';
+            const query = `SELECT * FROM ${table} WHERE id = '${id}'`;
+            pool.queryParam_None(query)
+                .then(userResult => {
+                    console.log(userResult);
+                    if (userResult.length == 0) {
+                        resolve({
+                            code: statusCode.BAD_REQUEST,
+                            json: authUtil.successFalse(responseMessage.NO_USER)
+                        });
+                        return;
+                    }
+                    const user = userResult[0];
+                    // 비밀번호 체크
+                    if (user.password != password) {
+                        resolve({
+                            code: statusCode.BAD_REQUEST,
+                            json: authUtil.successFalse(responseMessage.MISS_MATCH_PW)
+                        })
+                        return;
+                    }
+                    // 로그인 성공
+                    resolve({
+                        code: statusCode.OK,
+                        json: authUtil.successTrue(responseMessage.SIGN_IN_SUCCESS)
+                    });
                 })
-                return;
-            }
-            // 로그인 성공
-            resolve({
-                code:statusCode.OK,
-                json:authUtil.successTrue(responseMessage.SIGN_IN_SUCCESS) 
-            });
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
         })
     },
-    signup: (id, pwd, name, address) => {
-        return new Promise((resolve, reject) => {
-            // 아이디 중복 체크
-            if (userDB.filter(it => it.id == id).length > 0) {
-                resolve({
-                    code: statusCode.BAD_REQUEST,
-                    json: authUtil.successFalse(responseMessage.ALREADY_ID)
+    signup: ({
+        id,
+        password,
+        name,
+        email,
+        phone
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            const table = 'user';
+            const query = `SELECT * FROM ${table} WHERE id = '${id}'`;
+            pool.queryParam_None(query)
+                .then(userResult => {
+                    // 아이디 중복 체크
+                    if (userResult.length > 0) {
+                        resolve({
+                            code: statusCode.BAD_REQUEST,
+                            json: authUtil.successFalse(responseMessage.ALREADY_ID)
+                        })
+                        return;
+                    }
+                    const table = 'user';
+                    const fields = 'id, name, password, salt, email, phone';
+                    const questions = `?, ?, ?, ?, ?, ?`;
+                    const values = [id, name, password, ' ', email, phone];
+                    pool.queryParam_Parse(`INSERT INTO ${table}(${fields}) VALUES(${questions})`, values)
+                        .then(result => {
+                            const userId = result.insertId;
+                            resolve({
+                                code: statusCode.OK,
+                                json: authUtil.successTrue(responseMessage.SIGN_UP_SUCCESS, userId)
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            reject(err);
+                        });
                 })
-                return;
-            }
-
-            // 회원가입 성공
-            const user = {
-                id,
-                pwd,
-                name,
-                address
-            }
-            userDB.push(user);
-            console.log(userDB);
-            const result = user;
-
-            resolve({
-                code: statusCode.OK,
-                json: authUtil.successTrue(responseMessage.SIGN_UP_SUCCESS, result)
-            })
+                .catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
         })
     }
 };
