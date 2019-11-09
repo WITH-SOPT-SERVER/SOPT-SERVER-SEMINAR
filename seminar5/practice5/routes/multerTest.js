3920be6cabe8724b88735e3857c456d177a45b68
윤희성 <heesung6701@naver.com>
2019-11-09 21:51:33 +0900

    S3 Practice Code

--- seminar5/practice5/routes/multerTest.js
+++ seminar5/practice5/routes/multerTest.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const router = express.Router();
 
-const multer = require('multer')
+const multer = require('multer');
 const upload = multer({
     dest: 'uploads/'
-})
+});
 
-router.post('/profile', upload.single('avatar'), (req, res) => {
-    // req.file 은 `avatar` 라는 필드의 파일 정보입니다.
+router.post('/single', upload.single('image'), (req, res) => {
+    // req.file 은 `image` 라는 필드의 파일 정보입니다.
     // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
     console.log(req.file);
-    console.log(req.body.id);
+    console.log(req.body);
     res.send({
         file: req.file,
         body: req.body
     });
 })
 
-router.post('/photos/upload', upload.array('photos', 12), (req, res) => {
+router.post('/array', upload.array('photos', 4), (req, res) => {
     // req.files 는 `photos` 라는 파일정보를 배열로 가지고 있습니다.
+    // 이때 두 번째 파라미터인 4는 최대 이미지의 개수(maxCount)입니다.
     // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
     console.log(req.files);
     console.log(req.body);
@@ -29,20 +30,20 @@ router.post('/photos/upload', upload.array('photos', 12), (req, res) => {
 })
 
 var cpUpload = upload.fields([{
-    name: 'avatar',
+    name: 'thumbnail',
     maxCount: 1
 }, {
-    name: 'gallery',
+    name: 'images',
     maxCount: 8
 }])
 
-router.post('/cool-profile', cpUpload, (req, res, next) => {
+router.post('/fields', cpUpload, (req, res) => {
     // req.files는 (String -> Array) 형태의 객체 입니다.
     // 필드명은 객체의 key에, 파일 정보는 배열로 value에 저장됩니다.
     //
     // e.g.
-    //  req.files['avatar'][0] -> File
-    //  req.files['gallery'] -> Array
+    //  req.files['thumbnail'][0] -> File
+    //  req.files['images'] -> Array
     //
     // 텍스트 필드가 있는 경우, req.body가 이를 포함할 것입니다.
     console.log(req.files);
